379722469d5dd0ebab8b468719c8f3820c207506
Trashbot7274 <1w3r5yviper2077@gmail.com>
2025-10-13 10:46:31 -0400

    created deck and card classes

---
+++ src/blackjack/card.py
@@ -0,0 +1,14 @@
+
+class Card:
+    def __init__(self, value: int, suit: str):
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        return f'{self.value} of {self.suit}'
+    def get_blackjack_value(self):
+        if self.value == 1:
+            return 11
+        elif self.value >= 10:
+            return 10
+        return self.value
---
+++ src/blackjack/hand.py
@@ -0,0 +1,16 @@
+from src.blackjack.card import Card
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add(self, card: Card):
+        self.cards.append(card)
+    def value(self):
+        total = sum(card.get_blackjack_value() for card in self.cards)
+        number_aces = sum(1 for card in self.cards if card.value == 1)
+        while total > 21 and number_aces > 0:
+            total -= 10
+            number_aces -= 1
+        return total
